db12a190fe7fae1e5a00f3faeeaa91f58b239a64
Nyein Phyo Aung <nyeinphyoaung.edu@gmail.com>
2024-02-02 01:01:57 +0630

    adding to the firebase

--- firebase.js
+++ firebase.js
@@ -1,15 +1,21 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDmv6F4H7UKpGCdHddi2ZhzT_CliU33Ibs",
-  authDomain: "react-notes-7e95f.firebaseapp.com",
-  projectId: "react-notes-7e95f",
-  storageBucket: "react-notes-7e95f.appspot.com",
-  messagingSenderId: "400294071026",
-  appId: "1:400294071026:web:5b43a59c0f7621aa6a9e6b"
+  apiKey: "AIzaSyCXDKpVlhYYCCpDtAfSrJsItQ14DJZ3yK0",
+  authDomain: "note-app-1bdec.firebaseapp.com",
+  projectId: "note-app-1bdec",
+  storageBucket: "note-app-1bdec.appspot.com",
+  messagingSenderId: "626815977756",
+  appId: "1:626815977756:web:682735490e209f6bc5d2d9"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const notesCollection = collection(db, "notes");
+
